540161dd88260d222ca1340d570821e9334173ea
Gabriela Avila <78932145+ASGABY@users.noreply.github.com>
2021-02-25 14:03:42 -0600

    Update Homework1.c

--- Homework1.c
+++ Homework1.c
@@ -8,48 +8,59 @@ int main(){
     int i = 0;
     char strong[7]={"strong"};
 
-    while((c = getchar()) != EOF){
+    while((c = getchar()) != EOF)
+    {
 
-            if(c == '<'){
+            if(c == '<')
+            {
                     state = 0;
-                    for(s = 0; (c = getchar()) == strong[s]; s++){
-                            ;
+                    for(s = 0; (c = getchar()) == strong[s]; s++)
+                    {
+                            
                     }
                 if (s != 6){
-                while( (c = getchar()) != '>' ){
+                while( (c = getchar()) != '>' )
+                {
                         ;
                 }
             }
             else {
-                while( c != '>' ){
+                while( c != '>' )
+                {
                         ;
                 }
             }
                 ++ i;
-            if( (c = getchar()) == '<'){
+            if( (c = getchar()) == '<')
+            {
                 state = 1;
-                while( (c = getchar()) != '>' ){
+                while( (c = getchar()) != '>' )
+                {
                     ;
                 }
                 i++;
             }
 
         }
-        if(c != '<' && c!= '>'){
+        if(c != '<' && c!= '>')
+        {
             i = 0;
         }
-           if(state == 0 && i == 0 && s != 6){
+           if(state == 0 && i == 0 && s != 6)
+           {
             putchar(c);
             i = 0;
            }
            if(s == 6){
-                if(c == ' '){
+                if(c == ' ')
+                {
                     printf("_");
                     c = getchar();
                 }
             putchar(c);
            }
-        if ( c == '>' && s == 6){
+        if ( c == '>' && s == 6)
+        {
             s=0;
         }
     }
