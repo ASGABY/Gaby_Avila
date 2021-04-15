1dfc38f368897fe8b95c8687f7bb7649b65feab0
Gabriela Avila <78932145+ASGABY@users.noreply.github.com>
2021-04-15 08:26:52 -0500

    Create Homework 4.c
    Based on the book and the feedback of the repository of Hector Mtz.

---
+++ Homework 4.c
@@ -0,0 +1,63 @@
+#include <stdio.h>  // printf
+#include <stdlib.h> // atof
+#include <ctype.h>
+#include "calc.h"
+
+
+#define MAXOF 100
+
+int main() {
+	int type;
+	double op2;
+	char s[MAXOF];
+
+	int assign = 0;
+
+	while ((type = getop(s)) != EOF) {
+		switch (type) {
+			case NUMBER:
+				push(atof(s));
+				break;
+			case '+':
+				push(pop() + pop());
+				break;
+			case '*':
+				push(pop() * pop());
+				break;
+			case '-':
+				op2 = pop();
+				push(pop() - op2);
+				break;
+			case '/': 
+				op2 = pop();
+				if (op2 != 0.0) {
+					push (pop() / op2);
+				} else {
+					printf("error: zero divisor\n");
+				}
+				break;
+			case '=':
+				assign = 1;
+				break;
+
+
+			case '\n':
+				if (assign) {
+					
+				} else {
+					printf("\t%.8g\n", pop());
+				}
+				break;
+			default: 
+				// Por aquí podemos poner las asignaciones
+				if (isalpha(type)) {
+					pushlet(type);
+				} else {
+					printf("error: unknown command %s\n", s);
+				}
+
+				break;
+		}
+	}
+	return 0;
+}
