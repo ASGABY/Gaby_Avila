e927ab1bf07124c51e132631a24ebece861df961
Gabriela Avila <78932145+ASGABY@users.noreply.github.com>
2021-04-15 08:24:20 -0500

    Create Cal.h
    I based this on the book content

---
+++ Cal.h
@@ -0,0 +1,12 @@
+#define NUMBER '0'
+void push(double);
+double pop(void);
+int getop(char []);
+int getch(void);
+void ungetch(int);
+
+
+int getlet(void);
+void ungetlet(int);
+void pushlet(char);
+char poplet(void);
